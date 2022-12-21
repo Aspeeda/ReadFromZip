c71eaf8e52d4b64e6bb80161e5b874617a6f27d7
DESKTOP-T1P4KAC\User <aspeeda@yandex.ru>
2022-12-21 19:50:41 +0500

    model, zip

---
+++ src/test/java/tests/model/modelUser.java
@@ -0,0 +1,23 @@
+package tests.model;
+
+import java.util.ArrayList;
+
+public class modelUser {
+    public static class User {
+        public int id;
+        public String name;
+        public String username;
+        public Address address;
+        public ArrayList<Cars> cars;
+
+        public static class Address {
+            public String street;
+            public String suite;
+            public String city;
+        }
+        public static class Cars {
+            public String name;
+            public ArrayList<String> models;
+        }
+    }
+}
